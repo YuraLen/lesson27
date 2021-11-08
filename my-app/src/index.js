5964d9e5618f626e6217430f904c4bca1311d2c1
YuraLen <92933581+YuraLen@users.noreply.github.com>
2021-11-08 18:16:43 +0200

    3 задание | Выполнил примеры из занятия

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,17 +1,51 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
+import ReactDOM from "react-dom";
+import React from 'react'
 
-ReactDOM.render(
-  <React.StrictMode>
-    {/* <App /> */}
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
+function Greetings(p) {
+  return <h1>Hi, {p.name}, nice to meat you!</h1>
+}
+
+const userName = prompt("Как Вас зовут?");
+const userAge = prompt("Сколько Вам лет?");
+function whereYouNeedToGoTomorrow() {
+  if (userAge >= 2 && userAge <= 5) {
+    return <p>Завтра вам нужно идти в детский сад</p>;
+  } else if (userAge >= 6 && userAge <= 17) {
+    return <p>Завтра вам нужно идти в школу</p>;
+  } else if (userAge >= 18 && userAge <= 24) {
+    return <p>Завтра вам нужно идти в универ</p>;
+  } else if (userAge >= 24 && userAge <= 60) {
+    return <p>Завтра вам скорее всего нужно идти на работу</p>;
+  } else {
+    return <p style={{ color: "red" }}>Произошла ошибка</p>;
+  }
+}
+ReactDOM.render(
+  <div>
+    <Greetings class="GreetingStyle" name="Yura"/>
+    <h2>
+      Hi there. My name is {userName} and i'm {userAge} y.o.
+    </h2>
+
+    {whereYouNeedToGoTomorrow()}
+  </div>,
+  document.querySelector('#app')
+);
